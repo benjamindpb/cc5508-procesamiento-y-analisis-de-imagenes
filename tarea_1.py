1bc821f638ab4ad1f29e43ddc392f47342e6c74f
benjamindpb <benjamin.delpino@ug.uchile.cl>
2019-08-13 23:09:13 -0400

    se trabaja con los argumentos de entrada

--- tarea_1.py
+++ tarea_1.py
@@ -1,1 +1,21 @@
- 
+import argparse
+
+def hola():
+    print("hola chavales")
+    
+parser = argparse.ArgumentParser()
+parser.add_argument("--image", type = str, help = "indica la imagen sobre la que se trabajará", action = "store_true")
+parser.add_argument("--text", type = str, help = "indica el archivo que contiene el texto a codificar", action = "store_true")
+
+group = parser.add_mutually_exclusive_group()
+group.add_argument("--encode", help = "encodifica", action = "store_true")
+group.add_argument("--decode", help = "decodifica", action = "store_true")
+
+args = parser.parse_args()
+
+#si es el script principal
+if __name__ == '__main__':
+    if args.encode and args.image and args.text:
+        print(args)
+    if args.decode:
+        print("D")
