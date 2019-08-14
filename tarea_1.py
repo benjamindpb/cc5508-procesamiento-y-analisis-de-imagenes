cd56c39f5124cbaa0b8562cb790e19ca74d22dec
benjamindpb <benjamin.delpino@ug.uchile.cl>
2019-08-14 00:27:58 -0400

    argparse listo

--- tarea_1.py
+++ tarea_1.py
@@ -1,21 +1,20 @@
 import argparse
 
-def hola():
-    print("hola chavales")
-    
-parser = argparse.ArgumentParser()
-parser.add_argument("--image", type = str, help = "indica la imagen sobre la que se trabajará", action = "store_true")
-parser.add_argument("--text", type = str, help = "indica el archivo que contiene el texto a codificar", action = "store_true")
-
+parser = argparse.ArgumentParser(description = "Estenografía")
 group = parser.add_mutually_exclusive_group()
-group.add_argument("--encode", help = "encodifica", action = "store_true")
-group.add_argument("--decode", help = "decodifica", action = "store_true")
+group.add_argument("--encode", action = "store_true")
+group.add_argument("--decode", action = "store_true")
+    
+parser.add_argument("--image", nargs = 1, type = str)
+parser.add_argument("--text", nargs = 1, type = str)
+parser.add_argument("--nbits", nargs = 1, type = int)
 
 args = parser.parse_args()
 
+
 #si es el script principal
 if __name__ == '__main__':
-    if args.encode and args.image and args.text:
-        print(args)
+    if args.encode:
+        print("EnCODE")
     if args.decode:
-        print("D")
+        print("DECODE")
