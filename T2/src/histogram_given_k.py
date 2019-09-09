4e9da44934aa61d37569bc3eb592f1e45e7dbf60
benjamindpb <benjamin.delpino@ug.uchile.cl>
2019-09-09 00:24:44 -0300

    new folders. Terminal or nothing

--- T2/src/histogram_given_k.py
+++ T2/src/histogram_given_k.py
@@ -1,4 +1,5 @@
 import sys
+sys.path.append('../aux_functions')
 import matplotlib.pyplot as plt
 import numpy as np
 import basis
@@ -6,11 +7,11 @@ import pai_io
 import orientation_histograms as oh
 
 def getHistogram(K):
-    filenameA = 'dataset_1/BD_2/chair/180016.jpg'
+    filenameA = '../dataset_1/BD_2/chair/180016.jpg'
     imageA = pai_io.imread(filenameA, as_gray = True)
     hA = oh.compute_orientation_histogram(imageA, K)
-    
+
     plt.bar(range(K), height = hA)
     plt.show()
 
-getHistogram(36)
+getHistogram(100)
