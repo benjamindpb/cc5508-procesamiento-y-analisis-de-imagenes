ffd1a42073fd10e8c4077aee31248eae8eaae7c1
benjamindpb <benjamin.delpino@ug.uchile.cl>
2019-09-08 23:54:06 -0300

    too much things to commit :c

---
+++ T2/src/histogram_given_k.py
@@ -0,0 +1,16 @@
+import sys
+import matplotlib.pyplot as plt
+import numpy as np
+import basis
+import pai_io
+import orientation_histograms as oh
+
+def getHistogram(K):
+    filenameA = 'dataset_1/BD_2/chair/180016.jpg'
+    imageA = pai_io.imread(filenameA, as_gray = True)
+    hA = oh.compute_orientation_histogram(imageA, K)
+    
+    plt.bar(range(K), height = hA)
+    plt.show()
+
+getHistogram(36)
