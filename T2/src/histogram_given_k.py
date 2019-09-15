4ac091dc7531861b3090fa871a1ec520ce141bf6
benjamindpb <benjamin.delpino@ug.uchile.cl>
2019-09-15 17:17:08 -0300

    se modifican las carpetas

--- T2/src/histogram_given_k.py
+++ T2/src/histogram_given_k.py
@@ -1,17 +1,16 @@
 import sys
-sys.path.append('../aux_functions')
 import matplotlib.pyplot as plt
 import numpy as np
 import basis
 import pai_io
 import orientation_histograms as oh
 
-def getHistogram(K):
-    filenameA = '../dataset_1/BD_2/chair/180016.jpg'
-    imageA = pai_io.imread(filenameA, as_gray = True)
-    hA = oh.compute_orientation_histogram(imageA, K)
+silla = '/home/nenjamib/Escritorio/Primavera 2019/Procesamiento y Analisis de Imagenes/Tareas/cc5508-tareas/T2/dataset_1/BD_2/chair/180016.jpg'
 
+def getHistogram(image_path, K):
+    imageA = pai_io.imread(image_path, as_gray = True)
+    hA = oh.compute_orientation_histogram(imageA, K)
     plt.bar(range(K), height = hA)
     plt.show()
 
-getHistogram(100)
+getHistogram(silla, 100)
