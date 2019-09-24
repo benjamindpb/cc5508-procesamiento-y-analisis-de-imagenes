fd9018f7b3fe341c074772c06c1306b9bcb598f7
benjamindpb <benjamin.delpino@ug.uchile.cl>
2019-09-23 23:42:59 -0300

    se modifica HO  para que reciba path o matriz de imagen

--- T2/src/histogram_given_k.py
+++ T2/src/histogram_given_k.py
@@ -5,23 +5,32 @@ import pai_io
 import orientation_histograms as oh
 
 delfin = '/home/nenjamib/Escritorio/Primavera 2019/Procesamiento y Analisis de Imagenes/Tareas/cc5508-tareas/T2/dataset_1/BD_2/dolphin/240003.jpg'
+# print(type(delfin))
+delfin2 = pai_io.imread(delfin, as_gray = True)
+# print(type(delfin2))
+# print(type(delfin2) == np.ndarray)
 
 """
 Histrograma de Orientaciones
 
-Esta funcion recibe el path de la imagen y un valor de cuantizacion,
-y devuelve el histograma de orientaciones correspondiente (OH)
+Esta funcion recibe el path o la matriz correspondiente a la imagen y un valor de cuantizacion, y devuelve el histograma de orientaciones correspondiente (HO)
 
 Parametros
 ----------
-image_path: path de la imagen
+image_path: path o matriz de la imagen
 K: valor de cuantizacion
 """
 
-def getHistogram(image_path, K):
-    image = pai_io.imread(image_path, as_gray = True)
-    print(image)
-    hA = oh.compute_orientation_histogram(image, K)
-    plt.bar(range(K), height = hA)
-    plt.show()
-
+def getHistogram(image, K):
+    #si el tipo de im
+    if(type(image) == np.ndarray):
+        hA = oh.compute_orientation_histogram(image, K)
+        plt.bar(range(K), height = hA)
+        plt.show()
+    else:
+        img = pai_io.imread(image, as_gray = True)
+        hA = oh.compute_orientation_histogram(img, K)
+        plt.bar(range(K), height = hA)
+        plt.show()
+    
+#getHistogram(delfin2, 36)
