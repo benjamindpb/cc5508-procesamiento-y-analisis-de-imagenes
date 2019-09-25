e783400b23bf1c91a24833e915f740dc8d133e3f
benjamindpb <benjamin.delpino@ug.uchile.cl>
2019-09-25 00:46:38 -0300

    HELO funcionando correctamente (hope so)

--- T2/src/helo_histogram.py
+++ T2/src/helo_histogram.py
@@ -4,6 +4,7 @@ import scipy.ndimage.filters as nd_filters
 import basis
 import pai_io
 import orientation_histograms as oh
+import histogram_given_k as ho
 
 """
 Histograma de Orientaciones Locales
@@ -18,12 +19,38 @@ B: cantidad de bloques en los que se dividirá la imagen
 K: valor de cuantizacion
 """
 
+delfin = '/home/nenjamib/Escritorio/Primavera 2019/Procesamiento y Analisis de Imagenes/Tareas/cc5508-tareas/T2/dataset_1/BD_2/dolphin/240003.jpg'
+
 def helo_histogram(image_path, B, K):
-    image = pai_io.imread(image_path)
-    L_x = np.zeros(B)
-    L_y = np.zeros(B)
+    image = pai_io.imread(image_path, as_gray = True) #matriz que representa la imagen
+
+    L_x = np.zeros((B,B)) #conjunto de gradientes locales c/r a x
+    L_y = np.zeros((B,B)) #conjunto de gradientes locales c/r a y
+    ang = np.zeros((B,B))
+
     kernel_x = np.array([[-1,0,1], [-2,0,2],[-1,0,1]])
-    g_x = nd_filters.convolve(image_path.astype(np.float32), kernel_x, mode = 'constant', cval = 0)
     kernel_y = np.transpose(kernel_x)
-    g_y = nd_filters.convolve(image_path.astype(np.float32), kernel_y, mode = 'constant', cval = 0)
+
+    g_x = nd_filters.convolve(image, kernel_x, mode = 'constant', cval = 0.0)
+    g_y = nd_filters.convolve(image, kernel_y, mode = 'constant', cval = 0.0)
+    
+    height = np.shape(image)[0] #nro de pixeles de largo
+    width = np.shape(image)[1] #nro de pixeles de ancho
+    
+    for i in range(height):
+        for j in range(width):
+            r = round((i/height) * (B - 1))
+            s = round((j/width) * (B - 1))
+            #print(i, j)
+            L_x[r][s] += (g_x[i][j])**2 - (g_y[i][j])**2
+            L_y[r][s] += 2 * (g_x[i][j]) * (g_y[i][j])
+
+    for r in range(B):
+        for s in range(B):
+            ang[r,s] = np.arctan2(L_y[r,s], L_x[r,s]) * 0.5
+
+    ho.getHistogram(ang, K)
+    
+
+
 
