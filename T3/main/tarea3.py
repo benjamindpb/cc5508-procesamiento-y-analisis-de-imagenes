a762cb1ae92c2da251014d2dd108617ee8999528
bdelpino <benjamin.delpino@ug.uchile.cl>
2019-11-29 00:30:53 -0300

    implementacion de SIFT y creacion de carpeta de output

--- T3/main/tarea3.py
+++ T3/main/tarea3.py
@@ -3,19 +3,44 @@ import numpy as np
 
 if __name__== '__main__':
     
-    img1 = cv2.imread('../img/caso_2/1a.jpg')
-    img2 = cv2.imread('../img/caso_2/2a.jpg')
+    img1 = cv2.imread('../img/caso_3/3a.jpg')
+    img2 = cv2.imread('../img/caso_3/3b.jpg')
 
-    gray= cv2.cvtColor(img1,cv2.COLOR_BGR2GRAY)
+    gray1= cv2.cvtColor(img1,cv2.COLOR_BGR2GRAY)
+    gray2= cv2.cvtColor(img2,cv2.COLOR_BGR2GRAY)
+
+    """
+    xfeatures2d.SIFT_create()
+    creacion de un objeto de tipo SIFT
+    """
     sift = cv2.xfeatures2d.SIFT_create()
-    kp = sift.detect(gray)
-    print("n_kp: {}".format(len(kp)))
-    #kp, des = sift.compute(gray, kp)
-    cv2.drawKeypoints(gray,kp, img1, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-    cv2.imshow("img1", img1)
+    """
+    sift.detect()
+    funcion que encuentra los keypoints en la imagen.
 
-    cv2.waitKey()
-    cv2.destroyAllWindows()
+    Cada keypoint es una estructura especial que contiene muchos atributos
+    como sus coordenadas (x,y), el tamaño significativo del vecindario, el
+    angulo que especifica la orientacion, etc.
+    """
+    kp1 = sift.detect(gray1)
+    kp2 = sift.detect(gray2)
+
+    #kp, des = sift.compute(gray, kp)
+    """
+    cv2.drawKeypoints() dibuja los pequeños ciorculos donde se ubican 
+    los keypoints.
+    DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS es una flag que dibujará 
+    un círculo con el tamaño del keypoint e incluso mostrará su orientación
+    """
+    img_whit_kp1 = cv2.drawKeypoints(gray1,kp1, img1, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+    img_whit_kp2 = cv2.drawKeypoints(gray2,kp2, img2, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+
+    cv2.imwrite("..\imgOut\sift_kp1.jpg", img_whit_kp1)
+    cv2.imwrite("..\imgOut\sift_kp2.jpg", img_whit_kp2)
+
+
+
+    
 
     
 
