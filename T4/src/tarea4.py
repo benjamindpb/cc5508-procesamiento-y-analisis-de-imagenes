7dd9ad499dc1cc59381945e90d6c72d9014bd510
benjamindpb <benjamin.delpino@ug.uchile.cl>
2019-12-12 03:50:23 -0300

    recordando plt :c

--- T4/src/tarea4.py
+++ T4/src/tarea4.py
@@ -9,10 +9,25 @@ import basis
 if __name__ == "__main__":
 
     #imagen origen
-    imgOrigen = pai_io.imread("../img/hitler_face.jpg")
+    imgOrigen = pai_io.imread("../img/piñera_face.jpg")
     #imagen destino
-    imgDestino = pai_io.imread("../img/piñera_face.jpg")
+    imgDestino = pai_io.imread("../img/hitler_face.jpg")
+
+########################################################################
+    plt.figure()
+
+    plt.subplot(1, 2, 1)
+    plt.imshow(imgOrigen)
+    plt.axis('off')
+    plt.title("Piñera")
+
+    plt.subplot(1, 2, 2)
+    plt.imshow(imgDestino)
+    plt.axis('off')
+    plt.title("Hitler")
+
     plt.show()
+########################################################################
 
     #ruta del archivo que contiene los pares de lineas de ref
     rutaArchivo = "../txt/refLines.txt"
