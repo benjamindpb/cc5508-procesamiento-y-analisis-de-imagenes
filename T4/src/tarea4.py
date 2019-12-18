d01d27368f5cdcd1523268b22d4b43b0aa85e1be
benjamindpb <benjamin.delpino@ug.uchile.cl>
2019-12-18 11:46:30 -0300

    se comienza a trabajr con el algoritmo

--- T4/src/tarea4.py
+++ T4/src/tarea4.py
@@ -7,25 +7,38 @@ import cv2
 
 #imgOrigen, imgDestino, archivoLineas, N
 def morph(rutaImgOrigen, rutaImgDestino, rutaArchivoLineas, N):
-    images = []
+
+    #ruta del archivo que contiene los pares de lineas de ref
+    rutaArchivoDeLineas = "../txt/refLines.txt"
+    archivoDeLineas  = open(rutaArchivoDeLineas, 'r')
+    lineas = archivoDeLineas.read().splitlines()
+    
+    paresDeLineas = []
+    for par in lineas:
+        L = par[3:].split(",")
+        for i in range(len(L)):
+            L[i] = int(L[i])
+        paresDeLineas.append(L)
+
+    listaPuntos=[]
+    for lineas in paresDeLineas:
+        for i in range(len(lineas)):
+            if i%2 == 0:
+                P=[]
+                P.append(lineas[i])
+            else:
+                P.append(lineas[i])
+                listaPuntos.append(P)
+
+    listaPuntos = np.array(listaPuntos)
+    print(listaPuntos)
+
     #imagen origen
     imgOrigen = pai_io.imread(rutaImgOrigen)
-    print(imgOrigen.shape)
     #imagen destino
     imgDestino = pai_io.imread(rutaImgDestino)
-    print(imgDestino.shape)
 
-    images.append(imgOrigen)
-    images.append(imgDestino)
-
-    alto, ancho , capas = images.shape
-    size = (ancho, alto)
-
-    out = cv2.VideoWriter("morphing.avi", cv2.VideoWriter_fourcc(*'DIVX'), 15, size)
-
-    for i in range(len(images)):
-        out.write(images[i])
-    out.release()
+    largo, ancho, dim = imgDestino.shape
 
 ########################################################################
     # plt.figure()
@@ -43,31 +56,10 @@ def morph(rutaImgOrigen, rutaImgDestino, rutaArchivoLineas, N):
     # plt.show()
     #hola()
 ########################################################################
-
-    #ruta del archivo que contiene los pares de lineas de ref
-    rutaArchivoDeLineas = "../txt/refLines.txt"
-    archivoDeLineas  = open(rutaArchivoDeLineas, 'r')
-    lineas = archivoDeLineas.read().splitlines()
     
-    paresDeLineas = []
-    for par in lineas:
-        L = par[3:].split(",")
-        for i in range(len(L)):
-            L[i] = int(L[i])
-        paresDeLineas.append(L)
-    
-    #print(paresDeLineas)
-
-    listaPuntos=[]
-    for lineas in paresDeLineas:
-        for i in range(len(lineas)):
-            if i%2 == 0:
-                P=[]
-                P.append(lineas[i])
-            else:
-                P.append(lineas[i])
-                listaPuntos.append(P)
-    print(listaPuntos)
+    for i in range(largo):
+        for j in range(ancho):
+            
     
 
 if __name__ == "__main__":
